f7608e499cff3218e60723b0f6666c05a21d1df6
girtonman <girtonman@gmail.com>
2020-08-09 21:14:21 -0500

    Updates

---
+++ precache-manifest.a40a41313db3acad33a61f2e2fc549dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b2b219a6a34ae2bfa9aebcaadaabe3",
+    "url": "/save-the-spire/index.html"
+  },
+  {
+    "revision": "ef911ca8700ee8dd6862",
+    "url": "/save-the-spire/static/css/main.a77e04bb.chunk.css"
+  },
+  {
+    "revision": "4a3d97c78366be466cd0",
+    "url": "/save-the-spire/static/js/2.8954114e.chunk.js"
+  },
+  {
+    "revision": "218094d1166866f0dc23cc28c95310b2",
+    "url": "/save-the-spire/static/js/2.8954114e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef911ca8700ee8dd6862",
+    "url": "/save-the-spire/static/js/main.2fd4f725.chunk.js"
+  },
+  {
+    "revision": "49616748008afc60a89c",
+    "url": "/save-the-spire/static/js/runtime-main.76c38e41.js"
+  }
+]);
